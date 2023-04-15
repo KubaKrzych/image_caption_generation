fd9716d5443be6106cac5e9a4cb3366cdeb88952
Kuba <krzychowiec.k@gmail.com>
2023-04-15 19:39:43 +0200

    Modify text_vectorization.py

--- app/text_vectorization.py
+++ app/text_vectorization.py
@@ -3,23 +3,44 @@ import pickle
 
 from load_data import get_clean_descriptions, load_file
 from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical, pad_sequences
+from numpy import array
+from load_data import get_feature_vector
+from text_preprocessing import to_bag_of_words
 
 def max_len(flatten_descriptions):
     return max((len(l) for l in flatten_descriptions))
 
 
-def rnn_data_gen():
-    return None
+def create_sequences(tokenizer, max_length, descriptions, photos, vocab_size):
+    photo_for_seq, input_sequences, output_for_seq = list(), list(), list()
+    # walk through each image identifier
+    for key, desc_list in descriptions.items():
+        # walk through each description for the image
+        for desc in desc_list:
+            # encode the sequence
+            seq = tokenizer.texts_to_sequences([desc])[0]
+            for i in range(1, len(seq)):
+                # split into input and output pair
+                in_seq, out_seq = seq[:i], seq[i]
+                # pad input sequence
+                in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                # encode output sequence
+                out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                # store
+                photo_for_seq.append(photos[key][0])
+                input_sequences.append(in_seq)
+                output_for_seq.append(out_seq)
+    return array(photo_for_seq), array(input_sequences), array(output_for_seq)
 
-def create_sequences_of_words():
-    
 
 filename = 'data/info_on_images/Flickr_8k.trainImages.txt'
 descriptions = get_clean_descriptions(load_file(filename), 'utils/descriptions.txt')
-
 flatten = [item for sub in descriptions.values() for item in sub]
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(flatten)
-pickle.dump(tokenizer, open('utils/vectorized_vocab.pkl', 'wb'))
 
+create_sequences(
+    tokenizer, max_len(flatten), descriptions, get_feature_vector(descriptions.keys(), 'utils/features.pkl'), len(to_bag_of_words(descriptions))+1)
+# pickle.dump(tokenizer, open('utils/vectorized_vocab.pkl', 'wb'))
